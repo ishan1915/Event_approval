b04e9aa09b48ab6ee553d124a917195e49ea6dbc
Ishan <102755360+ishan1915@users.noreply.github.com>
2024-07-02 16:32:35 +0530

    Delete calendar_project directory

--- calendar_project/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('calendar/', include('calendar_app.urls')),
-   # path('accounts/', include('django.contrib.auth.urls')),  # This includes login and logout URLs
-]
